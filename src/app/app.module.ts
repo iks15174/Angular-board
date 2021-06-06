c404171f14b9809b55ba5b75cdbaad290f75ab60
ParkJiHo <iks15174@gmail.com>
2021-06-06 23:05:20 +0900

    앵귤러 생각정리 글 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
-
-
-import { SignService } from "./service/rest-api/sign.service";
-import { UserlistService } from "./service/member/userlist.service";
-import { MyinfoService } from './service/rest-api/myinfo.service';
-import { BoardService } from './service/rest-api/board.service';
-import { GraphService } from './service/graph/graph.service';
 import { MaterialModule } from './modules/meterial/meterial.module';
 
 @NgModule({
@@ -26,11 +19,6 @@ import { MaterialModule } from './modules/meterial/meterial.module';
     MaterialModule,
   ],
   providers: [
-    SignService,
-    UserlistService,
-    MyinfoService,
-    BoardService,
-    GraphService
   ],
   bootstrap: [AppComponent]
 })
